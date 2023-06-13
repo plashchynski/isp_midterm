982ef9388430620c9c8658c2f4451edeb3d57d47
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-06-14 00:19:21 +0300

    Change tab size

--- sketch.js
+++ sketch.js
@@ -1,14 +1,4 @@
-let soundFile;
-
-// Control buttons
-let playButton;
-let pauseButton;
-let stopButton;
-let skipToStartButton;
-let skipToEndButton;
-let loopButton;
-let recordButton;
-
+let player;
 let mic;
 
 let playbackControlPanel;
@@ -17,41 +7,41 @@ let volumeSlider;
 
 // This function is called before setup() and is used to load external files
 function preload() {
-  // p5.js Sound library reference:
-  // https://p5js.org/reference/#/libraries/p5.sound
+    // p5.js Sound library reference:
+    // https://p5js.org/reference/#/libraries/p5.sound
 
-  // Define which audio formats are present in the assets folder
-  // As not all browsers support the same audio formats, p5.js will try to load
-  // the first format that is supported by the browser
-  soundFormats('mp3', 'wav');
-  player = loadSound('assets/sample');
+    // Define which audio formats are present in the assets folder
+    // As not all browsers support the same audio formats, p5.js will try to load
+    // the first format that is supported by the browser
+    soundFormats('mp3', 'wav');
+    player = loadSound('assets/sample');
 
-  // A sound will play only if it's not already playing
-  player.playMode('untilDone');
+    // A sound will play only if it's not already playing
+    player.playMode('untilDone');
 }
 
 function setup() {
-  createCanvas(500, 500);
-  background(180);
+    createCanvas(500, 500);
+    background(180);
 
-  // Create an AudioIn object to record audio from the microphone
-  mic = new p5.AudioIn();
+    // Create an AudioIn object to record audio from the microphone
+    mic = new p5.AudioIn();
 
-  // Create the upper panel of buttons
-  playbackControlPanel = new PlaybackControlPanel(player, mic);
-  playbackControlPanel.setup();
+    // Create the upper panel of buttons
+    playbackControlPanel = new PlaybackControlPanel(player, mic);
+    playbackControlPanel.setup();
 
-  // Create the volume slider
-  volumeSlider = createSlider(0, 2, 1, 0.01);
-  volumeSlider.position(380, 200);
-  volumeSlider.style('transform', 'rotate(270deg)');
+    // Create the volume slider
+    volumeSlider = createSlider(0, 2, 1, 0.01);
+    volumeSlider.position(380, 200);
+    volumeSlider.style('transform', 'rotate(270deg)');
 }
 
 function draw() {
-  playbackControlPanel.draw();
+    playbackControlPanel.draw();
 
-  // Master volume
-  text('master\nvolume', volumeSlider.x + 40, volumeSlider.y - 100);
-  fill(255,0,0,0);
-  rect(volumeSlider.x+10, volumeSlider.y-140, 90, 250);
+    // Master volume
+    text('master\nvolume', volumeSlider.x + 40, volumeSlider.y - 100);
+    fill(255,0,0,0);
+    rect(volumeSlider.x+10, volumeSlider.y-140, 90, 250);
 }
